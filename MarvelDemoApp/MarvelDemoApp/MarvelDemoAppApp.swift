2ba7cd1af93ae1f92ea0bc1e8b66e381fcb19486
David Buhauer <developer@dab-it.dk>
2024-05-22 08:53:42 +0200

    🎉 Added initial project

---
+++ MarvelDemoApp/MarvelDemoApp/MarvelDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelDemoAppApp.swift
+//  MarvelDemoApp
+//
+//  Created by David Buhauer on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
